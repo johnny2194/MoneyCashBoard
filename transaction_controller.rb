c7d39892cc01b02fe4e340083366241e173dfdbb
Johnny Watson <johnny@streetchange.org.uk>
2017-05-08 15:21:36 +0100

    Add total amount to table and update style.css

--- transaction_controller.rb
+++ transaction_controller.rb
@@ -3,9 +3,12 @@ require('sinatra/contrib/all')
 require('pry-byebug')
 require_relative('models/transaction.rb')
 
+
+
 # INDEX - READ  
 get '/transactions' do 
   @transactions = Transaction.all()
+  @total_amount = Transaction.total_amount()
   erb(:'transactions/index')
 end 
 
