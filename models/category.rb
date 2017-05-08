2f13b8870ce3db154763b41c50441dcc9265b59a
Johnny Watson <johnny@streetchange.org.uk>
2017-05-08 15:48:47 +0100

    Add save and find function to category class

--- models/category.rb
+++ models/category.rb
@@ -10,4 +10,18 @@ class Category
     @name = options['name']
   end
 
+  def save()
+    sql = "INSERT INTO categories ( name, logo_url ) VALUES ('#{ @name }') 
+    RETURNING *;"
+    categories_data = SqlRunner.run(sql)
+    @id = categories_data.first()['id'].to_i
+  end
+
+  def Category.find( id )
+    sql = "SELECT * FROM categories WHERE id=#{id};"
+    category = SqlRunner.run( sql )
+    result = Category.new( category.first )
+    return result
+  end
+
 end
