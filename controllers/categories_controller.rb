cec66aebf081a53be96a350bfeb51c2f5b47bc29
Johnny Watson <johnny@streetchange.org.uk>
2017-05-09 13:24:27 +0100

    Add method to controller to show all transactions by category id

--- controllers/categories_controller.rb
+++ controllers/categories_controller.rb
@@ -5,8 +5,9 @@ require_relative('../models/transaction.rb')
 require_relative('../models/category.rb')
 
 
-# INDEX - READ  
-get '/:category.name' do 
-  @categories = Category.all()
-  erb(:'categories/index')
-end 
+#  SHOW - READ
+
+get '/categories/:id' do 
+  @categories = Category.find(params[:id])
+  erb(:'categories/show')
+end  
