0ac46418e1584eeb608a3ed1c9b276cb060b36eb
Johnny Watson <johnny@streetchange.org.uk>
2017-05-09 13:20:34 +0100

    Create app.rb and hook in both controller and amened controller paths to suit

---
+++ app.rb
@@ -0,0 +1,8 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative('controllers/categories_controller')
+require_relative('controllers/transaction_controller')
+
+
+
+
--- controllers/transaction_controller.rb
+++ controllers/transaction_controller.rb
@@ -1,8 +1,8 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require('pry-byebug')
-require_relative('models/transaction.rb')
-require_relative('models/category.rb')
+require_relative('../models/transaction.rb')
+require_relative('../models/category.rb')
 
 
 
