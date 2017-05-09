0e6d4b4721a868bbf2debf5e7e1083c7d7598baf
Johnny Watson <johnny@streetchange.org.uk>
2017-05-09 13:11:09 +0100

    Add categories controller

---
+++ categories_controller.rb
@@ -0,0 +1,12 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require('pry-byebug')
+require_relative('models/transaction.rb')
+require_relative('models/category.rb')
+
+
+# INDEX - READ  
+get '/:category.name' do 
+  @categories = Category.all()
+  erb(:'categories/index')
+end 
