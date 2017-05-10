e5f11e13cf65d1df4e9c522d9b199c737e33396a
Johnny Watson <johnny@streetchange.org.uk>
2017-05-10 22:42:45 +0100

    Add methods to categories controller

--- controllers/categories_controller.rb
+++ controllers/categories_controller.rb
@@ -10,3 +10,16 @@ get '/categories' do
   @categories = Category.all()
   erb(:'categories/index')
 end 
+
+# NEW - CREATE  
+get '/categories/new' do
+  @categories= Category.all()
+  erb(:'categories/new')
+end 
+
+#CREATE - CREATE
+post '/categories' do
+  @categories = Category.new(params)
+  @categories.save()
+  redirect '/categories'
+end
