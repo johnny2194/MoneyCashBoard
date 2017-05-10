2c8e7678b7acca0dc9545f3ba87ad02d9919f0ef
Johnny Watson <johnny@streetchange.org.uk>
2017-05-10 11:43:36 +0100

    Add functions to bank class to find bank by id, all banks and all transaction where bank_id = id

--- models/bank.rb
+++ models/bank.rb
@@ -17,9 +17,35 @@ class Bank
   end
 
   def save()
-    sql = "INSERT INTO banks VALUES ( name, logo_url ) VALUES ('#{ @name }','#{ @logo_url }') 
+    sql = "INSERT INTO banks ( name, logo_url ) VALUES ('#{ @name }','#{ @logo_url }') 
     RETURNING *;"
     categories_data = SqlRunner.run(sql)
     @id = categories_data.first()['id'].to_i
+  end
+
+  def Bank.find( id )
+    sql = "SELECT * FROM banks WHERE id=#{id};"
+    bank = SqlRunner.run( sql )
+    result = Bank.new( bank.first )
+    return result
+  end
+
+  def Bank.all()
+    sql = "SELECT * FROM banks;"
+    banks = SqlRunner.run( sql )
+    result = banks.map { |bank| Bank.new( bank ) }
+    return result
+  end
+
+  def transactions_bank()
+    sql = "
+    SELECT * FROM transactions
+    WHERE bank_id = #{@id};"
+
+    transaction_hashes = SqlRunner.run(sql)
+    transaction_objects = transaction_hashes.map do |transaction_hash|
+      Transaction.new(transaction_hash)
+    end
+    return transaction_objects
   end
 end 
